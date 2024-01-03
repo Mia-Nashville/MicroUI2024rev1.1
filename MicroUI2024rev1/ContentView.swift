8f72618da3f3be24f9fa11ef40f221cf0b2fb93f
Satoshi550 <78008562+Satoshi550@users.noreply.github.com>
2024-01-03 16:19:00 -0600

    Initial Commit

---
+++ MicroUI2024rev1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MicroUI2024rev1
+//
+//  Created by Satoshi Mitsumori on 1/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MicroUI2024rev1/MicroUI2024rev1App.swift
@@ -0,0 +1,17 @@
+//
+//  MicroUI2024rev1App.swift
+//  MicroUI2024rev1
+//
+//  Created by Satoshi Mitsumori on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MicroUI2024rev1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
